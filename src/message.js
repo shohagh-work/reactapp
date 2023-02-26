8a9bebb0a1d1d7d072b2ee162c8ac4ab17f70555
shohaghpikt <85873344+shohaghpikt@users.noreply.github.com>
2023-02-26 18:01:54 +0600

    Update upto Props

--- src/message.js
+++ src/message.js
@@ -1,7 +1,26 @@
-const message = () => {
-  const name = 'Shohagh';
-  const age = 40;
-  return `name is ${name} and age is ${age}`;
-};
+import React from 'react';
 
-export default message;
+function Car() {
+  return (
+    <h2>
+      This is acar.
+    </h2>
+  );
+}
+function Garage() {
+  return (
+    <>
+      <h1>It is a garage</h1>
+      <Car />
+    </>
+  );
+}
+export default Garage;
+
+// const message = () => {
+//   const name = 'Shohagh';
+//   const age = 40;
+//   return `name is ${name} and age is ${age}`;
+// };
+
+// export default message;
