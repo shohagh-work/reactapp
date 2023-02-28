36b5bb14fbe929aac257c4017085c7c4551b19e9
shohagh <shohaghpikt@gmail.com>
2023-02-28 22:51:22 +0600

    just update

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -18,6 +18,19 @@ function Layout() {
         </ul>
       </nav>
       <Outlet />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
